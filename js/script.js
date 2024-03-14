04db697fc8de0ffd497c9063329bd5976149b336
Marinakostyrya <122kostyrya@gmail.com>
2024-03-14 13:29:22 +0200

    add slider

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $(".reviews__slider").owlCarousel({
+        items: 3,
+        // margin: -20,
+        dots: false,
+        stageClass: "owl-stage-flex",
+        center: true,
+        loop:true,
+        smartSpeed:800,
+        nav:false,
+
+
+
+    });
+    $(".reviews__buttons .owl-prev").click(function(){
+        $(".owl-prev").trigger("prev.owl.carousel")
+    });
+    
+    $(".reviews__buttons .owl-next").click(function(){
+        $(".owl-next").trigger("next.owl.carousel")
+    });
+    
+
+    
+});
